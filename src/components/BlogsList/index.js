8a24432775ad01182d89fa4d115977deb93f062d
somuravi1996 <ravi.somu617@gmail.com>
2023-02-22 07:15:49 +0530

    my blogs api data rendered

---
+++ src/components/BlogsList/index.js
@@ -0,0 +1,68 @@
+import Loader from 'react-loader-spinner'
+
+import {Component} from 'react'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+// const blogsData = [
+//   {
+//     id: 1,
+//     title: 'Blog 1',
+//     imageUrl: 'https://assets.ccbp.in/frontend/react-js/placeholder-1-img.png',
+//     avatarUrl: 'https://assets.ccbp.in/frontend/react-js/avatar-img.png',
+//     author: 'Author Name',
+//     topic: 'React.js',
+//   },
+//   {
+//     id: 2,
+//     title: 'Blog 2',
+//     imageUrl: 'https://assets.ccbp.in/frontend/react-js/placeholder-2-img.png',
+//     avatarUrl: 'https://assets.ccbp.in/frontend/react-js/avatar-img.png',
+//     author: 'Author Name',
+//     topic: 'React.js',
+//   },
+// ]
+
+class BlogsList extends Component {
+  state = {blogsData: [], isLoading: true}
+
+  componentDidMount() {
+    this.getBlogsData()
+  }
+
+  getBlogsData = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const data = await response.json()
+    console.log(data)
+    const updateData = data.map(eachItems => ({
+      id: eachItems.id,
+      imageUrl: eachItems.image_url,
+      avatarUrl: eachItems.avatar_url,
+      topic: eachItems.topic,
+      title: eachItems.title,
+      author: eachItems.author,
+    }))
+    console.log(updateData)
+    this.setState({blogsData: updateData, isLoading: false})
+  }
+
+  render() {
+    const {blogsData} = this.state
+    const {isLoading} = this.state
+    return (
+      <div className="blog-list-container">
+        {isLoading ? (
+          <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+        ) : (
+          blogsData.map(item => <BlogItem blogData={item} key={item.id} />)
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogsList
